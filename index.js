faaa9047c3c8be4a0899906e72414e490416aa59
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-24 21:29:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("text").remove()
+    let newP =document.createElement("p")
+    newP.innerHTML = 'This is really cool!'
+    document.querySelector('body').appendChild(newP) 
+})
